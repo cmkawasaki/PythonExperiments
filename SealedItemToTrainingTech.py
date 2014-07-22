2bbd46e6f2b5c9c1f67537ac1b781457bff92f67
Christopher Kawasaki <cmkawasaki@yahoo.com>
2014-07-22 16:37:52 -0700

    Finished Writing SealedItemToTrainingTech Script.

--- SealedItemToTrainingTech.py
+++ SealedItemToTrainingTech.py
@@ -2,6 +2,7 @@
 from sys import argv
 from os.path import exists
 from SealedItem import *
+from RTFWriter import *
 import re # Regular Expression Handler
 script, inputFileName = argv
 
@@ -33,6 +34,9 @@ if technique != "":
 print "Number of Techniques in file: %d" % len(listOfSealedItemBlocks)
 
 #Begin processing each technique, creating the formatted list of sub-techniques that we will need to create
+listOfTechniquesToMake = []
+dictOfTechniquesToMake = {}
+
 for sealedItemBlock in listOfSealedItemBlocks:
 	sealedItemObj = SealedItem(sealedItemBlock)
 	#print sealedItemObj.Title
@@ -43,12 +47,39 @@ for sealedItemBlock in listOfSealedItemBlocks:
 
 	for item in range(0,5):
 		if sealedItemObj.LevelPatternDictionary[SealedItemEnum[item]] != -1:
-			#Create Item Statblock Here
-			if sealedItemObj.Type == 'Sealed Item':
-				title = 'Craft ' + SealedItemEnum[item] + ' ' + sealedItemObj.Title
-			else:
-				title = 'Craft ' + SealedItemEnum[item] + ' ' + sealedItemObj.Title + ' ' + sealedItemObj.Type
-			print title
+			title = sealedItemObj.GenerateTitle(item)
+			listOfTechniquesToMake.append(title)
+			dictOfTechniquesToMake[title] = sealedItemObj.GenerateItem(item, previousItem)
 			previousItem = title
 
+#After For Loop completes, there should be a list of techniques and a dictionary with their loaded data.
+#Sort the Loop to determine printing order
+listOfTechniquesToMake.sort()
 
+writer = RTFWriter('.\\Output.rtf')
+
+for techTitle in listOfTechniquesToMake:
+	title, requireString, rank, jutsuClass, learnDC, successesNeeded, itemTitle, price = dictOfTechniquesToMake[techTitle]
+	#Print the Techniques using an RTF Writer in the Sorted Order
+	writer.WriteBold(title)
+	writer.InsertReturn()
+	writer.WriteItalics(requireString)
+	writer.InsertReturn()
+	writer.WriteBold('Rank: ')
+	writer.Write(str(rank) + ' (' + jutsuClass + '); ')
+	writer.WriteBold('Learn DC: ')
+	writer.Write(str(learnDC) + ', ' + str(successesNeeded) + ' successes; ')
+	writer.WriteBold('Components:')
+	writer.Writeline(' X, Mas')
+	writer.Write('The user gains the knowledge required to craft a ')
+	writer.WriteItalics(itemTitle)
+	writer.Writeline('. See Ninja Tools section for details. The subtype is either Ninjutsu if learned with Craft (calligraphy) or Fuinjutsu if learned with Fuinjutsu. ')
+	writer.InsertReturn()
+	writer.WriteItalics('Mastery')
+	writer.InsertReturn()
+	writer.Writeline('TO BE WRITTEN')
+	writer.WriteBold('Expendable Components: ')
+	writer.Write('Arcane crystals sufficient required to make a couple sample seals (%s Ryo)' % ("{:,}".format(price)))
+	writer.InsertReturn()
+	writer.InsertReturn()
+writer.Close()
